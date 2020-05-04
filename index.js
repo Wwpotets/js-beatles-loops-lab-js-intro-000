ce36ee0be4758aa6b6d4d83327c48f7dc4ec7587
Corey Lynch <bearinawolfpack@gmail.com>
2020-05-04 15:34:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  var music = musicians.length;
+  var instr = instruments.length;  
+  
+  for (let num = 0; num <= music; num++) {
+    arr.push(musicians[num] + instruments[num]);
+  }
+  return arr; 
+}
